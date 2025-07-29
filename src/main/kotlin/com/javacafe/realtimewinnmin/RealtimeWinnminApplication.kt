3cbd2c904a51e5fbff1719591ae7aa6e5a74afdd
유승민 <ojysep9987@gmail.com>
2025-07-29 18:11:08 +0900

    feat : project init setting

---
+++ src/main/kotlin/com/javacafe/realtimewinnmin/RealtimeWinnminApplication.kt
@@ -0,0 +1,11 @@
+package com.javacafe.realtimewinnmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealtimeWinnminApplication
+
+fun main(args: Array<String>) {
+    runApplication<RealtimeWinnminApplication>(*args)
+}
---
+++ src/test/kotlin/com/javacafe/realtimewinnmin/RealtimeWinnminApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.javacafe.realtimewinnmin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealtimeWinnminApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
