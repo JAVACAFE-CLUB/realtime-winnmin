dcc08cd689e195885163b1396339dfe51ee69cf9
유승민 <ojysep9987@gmail.com>
2025-08-04 22:43:44 +0900

    fix : 단순 success,fail 이 아닌 HTTP 상태 코드로 표현하도록 수정

--- src/main/kotlin/com/javacafe/realtimewinnmin/common/dto/ApiResponse.kt
+++ src/main/kotlin/com/javacafe/realtimewinnmin/common/dto/ApiResponse.kt
@@ -1,20 +1,36 @@
 package com.javacafe.realtimewinnmin.common.dto
 
+import org.springframework.http.HttpStatus
 import java.time.LocalDateTime
 
 data class ApiResponse<T>(
-    val success: Boolean,
-    val data: T?,
+    val code: HttpStatus,
+    val data: T? = null,
     val message: String,
     val timestamp: LocalDateTime = LocalDateTime.now()
 ) {
     companion object {
-        fun <T> success(data: T?, message: String = "성공"): ApiResponse<T> {
-            return ApiResponse(success = true, data = data, message = message)
+        fun <T> success(
+                code: HttpStatus = HttpStatus.OK,
+                data: T,
+                message: String = "성공"
+        ): ApiResponse<T> {
+            return ApiResponse(
+                code = code,
+                data = data,
+                message = message
+            )
         }
 
-        fun <T> error(message: String = "오류가 발생했습니다"): ApiResponse<T> {
-            return ApiResponse(success = false, data = null, message = message)
+        fun error(
+            code: HttpStatus = HttpStatus.BAD_REQUEST,
+            message: String = "오류가 발생했습니다"
+        ): ApiResponse<Nothing> {
+            return ApiResponse(
+                code = code,
+                data = null,
+                message = message
+            )
         }
     }
 }
