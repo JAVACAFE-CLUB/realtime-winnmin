79c58328df2a1141361b2699edabc71f369cba14
유승민 <ojysep9987@gmail.com>
2025-08-05 20:42:06 +0900

    fix : 불필요한 code 제거

--- src/main/kotlin/com/javacafe/realtimewinnmin/common/dto/ApiResponse.kt
+++ src/main/kotlin/com/javacafe/realtimewinnmin/common/dto/ApiResponse.kt
@@ -4,30 +4,25 @@ import org.springframework.http.HttpStatus
 import java.time.LocalDateTime
 
 data class ApiResponse<T>(
-    val code: HttpStatus,
     val data: T? = null,
     val message: String,
     val timestamp: LocalDateTime = LocalDateTime.now()
 ) {
     companion object {
         fun <T> success(
-                code: HttpStatus = HttpStatus.OK,
                 data: T,
                 message: String = "성공"
         ): ApiResponse<T> {
             return ApiResponse(
-                code = code,
                 data = data,
                 message = message
             )
         }
 
         fun error(
-            code: HttpStatus = HttpStatus.BAD_REQUEST,
             message: String = "오류가 발생했습니다"
         ): ApiResponse<Nothing> {
             return ApiResponse(
-                code = code,
                 data = null,
                 message = message
             )
