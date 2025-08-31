3e6bd1aad951826988cbd85751a58312478147a5
유승민 <ojysep9987@gmail.com>
2025-08-31 11:24:51 +0900

    feat : 수집 시스템 초기 세팅

---
+++ rtw-collector/src/main/kotlin/com/javacafe/rtwcollector/RtwCollectorApplication.kt
@@ -0,0 +1,11 @@
+package com.javacafe.rtwcollector
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RtwCollectorApplication
+
+fun main(args: Array<String>) {
+    runApplication<RtwCollectorApplication>(*args)
+}
---
+++ rtw-collector/src/test/kotlin/com/javacafe/rtwcollector/RtwCollectorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.javacafe.rtwcollector
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RtwCollectorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
