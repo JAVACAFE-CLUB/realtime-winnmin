63bddd68dfe5c54b67d18b2339b2d7fecfbad345
유승민 <ojysep9987@gmail.com>
2025-08-31 13:42:16 +0900

    feat : 색인 시스템 추가

---
+++ rtw-index/src/main/kotlin/com/javacafe/rtwindex/RtwIndexApplication.kt
@@ -0,0 +1,11 @@
+package com.javacafe.rtwindex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RtwIndexApplication
+
+fun main(args: Array<String>) {
+	runApplication<RtwIndexApplication>(*args)
+}
---
+++ rtw-index/src/test/kotlin/com/javacafe/rtwindex/RtwIndexApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.javacafe.rtwindex
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RtwIndexApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
