16d96e38bb2d247390c31740dc18c6391a0651a6
유승민 <ojysep9987@gmail.com>
2025-09-14 21:13:46 +0900

    feat : 코루틴, http 및 직렬화 config 빈 설정 추

---
+++ rtw-collector/src/main/kotlin/com/javacafe/rtwcollector/common/config/HttpClientConfig.kt
@@ -0,0 +1,22 @@
+package com.javacafe.rtwcollector.common.config
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.net.http.HttpClient
+import java.time.Duration
+
+@Configuration
+class HttpClientConfig(
+    @Qualifier("ioThreadPoolTaskExecutor") private val ioExecutor: ThreadPoolTaskExecutor
+) {
+
+    @Bean
+    fun httpClient(): HttpClient {
+        return HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(10))
+            .executor(ioExecutor.threadPoolExecutor) // 스프링이 관리하는 스레드풀 사용
+            .build()
+    }
+}
---
+++ rtw-collector/src/main/kotlin/com/javacafe/rtwcollector/common/config/JacksonConfiguration.kt
@@ -0,0 +1,23 @@
+package com.javacafe.rtwcollector.common.config
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+@Configuration
+class JacksonConfiguration {
+
+    @Bean
+    @Primary
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper().apply {
+            registerModule(KotlinModule.Builder().build())
+            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        }
+    }
+}
