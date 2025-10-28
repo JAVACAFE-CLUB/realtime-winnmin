4af39afcd0e9e007bd0e5ccf28dab881a8ab1c21
유승민 <ojysep9987@gmail.com>
2025-10-28 21:38:23 +0900

    doc : logback 설정 추

--- rtw-dataclean/src/test/kotlin/com/javacafe/rtwdataclean/RtwDatacleanApplicationTests.kt
+++ rtw-dataclean/src/test/kotlin/com/javacafe/rtwdataclean/RtwDatacleanApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.javacafe.rtwdataclean
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class RtwDatacleanApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+//package com.javacafe.rtwdataclean
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class RtwDatacleanApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
